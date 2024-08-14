53f69508e5774ef648a1faf2e639b06906dd4f46
Vinicius Hashizume <viniciushashizume26@gmail.com>
2024-08-14 01:10:15 -0300

    torch

---
+++ Projetos/pytorch/dataset.py
@@ -0,0 +1,26 @@
+from typing import Any
+from torch.utils.data import Dataset
+import torchvision.datasets as datasets
+from torch import nn, relu
+
+class DataSet(Dataset):
+    def __init__(self, train=False, transform = None):
+        self.database = datasets.MNIST(root='./data', train = train, download = True, transform = transform) #se o diretorio nao existir, sera criado a partir do 'download'
+        #transform indica as transformacoes que os dados inseridos serão submetidos
+    def __getitem__(self, index):
+        data, target = self.dataset[index] #conforme o indice passado como parametro, sera retornado o dado e a qual classe ele perterce
+        return data, target
+    def __len__(self): #retorna o tamanho do dataset
+        return len(self.dataset)
+    
+class Model(nn.Module):
+    def __init__(self, inputsize, hidden1, hidden2, outputsize):
+        super(Model, self).__init__()
+        self.entry = nn.Linear(inputsize, hidden1)
+        self.hidden = nn.Linear(hidden1,hidden2)
+        self.out = nn.Linear(hidden2,outputsize)
+    def forward (self,x):
+        out = relu(self.entry(x))
+        out = relu(self.hidden(out))
+        out = self.out(x)
+        return out
